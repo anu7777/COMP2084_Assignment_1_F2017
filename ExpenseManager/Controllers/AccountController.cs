433f3262d22ca0b6b599862838691da9b8159fe9
kilvishatwal <kilvish.atwal@gmail.com>
2017-10-10 22:34:00 +0530

    Completed COMP2084

---
+++ ExpenseManager/Controllers/AccountController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ExpenseManager;
+
+namespace ExpenseManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        private DatabaseEntities db = new DatabaseEntities();
+
+        // GET: /Account/
+        public ActionResult Index()
+        {
+            var accounts = db.Accounts.Include(a => a.Friend);
+            return View(accounts.ToList());
+        }
+
+        // GET: /Account/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: /Account/Create
+        public ActionResult Create()
+        {
+            ViewBag.Friend_Id = new SelectList(db.Friends, "Id", "Name");
+            return View();
+        }
+
+        // POST: /Account/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Amount,SpentOn,Info,Friend_Id")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accounts.Add(account);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Friend_Id = new SelectList(db.Friends, "Id", "Name", account.Friend_Id);
+            return View(account);
+        }
+
+        // GET: /Account/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Friend_Id = new SelectList(db.Friends, "Id", "Name", account.Friend_Id);
+            return View(account);
+        }
+
+        // POST: /Account/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Amount,SpentOn,Info,Friend_Id")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Friend_Id = new SelectList(db.Friends, "Id", "Name", account.Friend_Id);
+            return View(account);
+        }
+
+        // GET: /Account/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: /Account/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Account account = db.Accounts.Find(id);
+            db.Accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
